fad3157ee77ad221f8594141fe6867fe7bf04524
Anwesh43 <anweshthecool0@gmail.com>
2020-07-03 23:06:27 +0530

    adding CornerSquare class

--- cornersquaresview/src/main/java/com/anwesh/uiprojects/cornersquaresview/CornerSquaresView.kt
+++ cornersquaresview/src/main/java/com/anwesh/uiprojects/cornersquaresview/CornerSquaresView.kt
@@ -152,4 +152,27 @@ class CornerSquaresView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class CornerSquare(var i : Int, val state : State = State()) {
+
+        private var curr : CSNode = CSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
